18da1ddba97443f2e9da7c4dc7dcc38b0c95d993
TusharGupta01 <tushar.behror@gmail.com>
2016-05-09 10:02:01 +0530

    commented femto

--- femto.c
+++ femto.c
@@ -7,23 +7,23 @@
 char buf[512];
 
 void
-cat(int fd,char* filename)
+femto(int fd,char* filename)
 {
   int n;
   int fp;
   
-  if ((fp = open(filename, O_CREATE | O_RDWR)) < 0) {
-    printf(1, "counter_init: error initializing file: %s\n", filename);
+  if ((fp = open(filename, O_CREATE | O_RDWR)) < 0) {                        // Open the file to be edited or in which you have to write 
+    printf(1, "editor: error initializing file: %s\n", filename);            // your input
     exit();
   }
 
-  while((n = read(fd, buf, sizeof(buf))) > 0){
-    // write(1, buf, n);
+  while((n = read(fd, buf, sizeof(buf))) > 0){                               // if there is some thing on the terminal it will get written 
+    // write(1, buf, n);                                                     // in file
     printf(fp, "%s", buf);
   }
   close(fp);
   if(n < 0){
-    printf(1, "cat: read error\n");
+    printf(1, "editor: read error\n");                                       // error message
     exit();
   }
 }
@@ -33,10 +33,10 @@ main(int argc, char *argv[])
 {
 
   if(argc <= 2){
-    char* filename = "default.txt";                                              
+    char* filename = "default.txt";                                           // Defalut file if user doesn't provide any file
     if(argv[1])
       filename = argv[1]; 
-    cat(0,filename);
+    femto(0,filename);                                                        // Calling the femto function 
     exit();
   }
   exit();
